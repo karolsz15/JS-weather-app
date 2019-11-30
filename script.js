276c4f972172b9b4d4962833d8348b5c7c1f96c9
Karol Szpek <56266637+karolsz15@users.noreply.github.com>
2019-11-30 11:53:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,35 +15,35 @@ var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
-var modal5 = document.getElementById("myModal5");
+//var modal5 = document.getElementById("myModal5");
 
 // Get the button that opens the modal
 var btn1 = document.getElementsByClassName("img")[0];
 var btn2 = document.getElementsByClassName("img")[1];
 var btn3 = document.getElementsByClassName("img")[2];
 var btn4 = document.getElementsByClassName("img")[3];
-var btn5 = document.getElementsByClassName("img")[4];
+//var btn5 = document.getElementsByClassName("img")[4];
 
 // Get the <span> element that closes the modal
 var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
 var span4 = document.getElementsByClassName("close4")[0];
-var span5 = document.getElementsByClassName("close5")[0];
+//var span5 = document.getElementsByClassName("close5")[0];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {modal1.style.display = "block";};
 btn2.onclick = function() {modal2.style.display = "block";};
 btn3.onclick = function() {modal3.style.display = "block";};
 btn4.onclick = function() {modal4.style.display = "block";};
-btn5.onclick = function() {modal5.style.display = "block";};
+//btn5.onclick = function() {modal5.style.display = "block";};
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function() {modal1.style.display = "none";};
 span2.onclick = function() {modal2.style.display = "none";};
 span3.onclick = function() {modal3.style.display = "none";};
 span4.onclick = function() {modal4.style.display = "none";};
-span5.onclick = function() {modal5.style.display = "none";};
+//span5.onclick = function() {modal5.style.display = "none";};
 
 
 // When the user clicks anywhere outside of the modal, close it
@@ -60,9 +60,9 @@ window.onclick = function(event) {
     if (event.target == modal4) {
         modal4.style.display = "none";
     }
-    if (event.target == modal5) {
-        modal5.style.display = "none";
-    }
+    //if (event.target == modal5) {
+    //    modal5.style.display = "none";
+    //}
 };
 
 
@@ -81,7 +81,7 @@ function searchFutureWeather(searchTerm) {
         drawWeatherPattern(data, 16, '2nd');
         drawWeatherPattern(data, 24, '3rd');
         drawWeatherPattern(data, 32, '4th');
-        drawWeatherPattern(data, 39, '5th');
+        //drawWeatherPattern(data, 39, '5th');
 	})
 	.catch(function() {
 		// catch any errors
@@ -145,7 +145,7 @@ function getWeatherByLocation(lat, lon) {
         drawWeatherPattern(data, 16, '2nd');
         drawWeatherPattern(data, 24, '3rd');
         drawWeatherPattern(data, 32, '4th');
-        drawWeatherPattern(data, 39, '5th');
+        //drawWeatherPattern(data, 39, '5th');
 	})
 	.catch(function() {
 		// catch any errors
