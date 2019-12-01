9d467d76d6837db7cde12243eb44648635895898
Karol Szpek <56266637+karolsz15@users.noreply.github.com>
2019-12-01 19:11:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,42 @@
 // Id number needed to access the API
-let appId = '4ec34d45c36f6fee7fb9427c490b032c';
+const appId = '4ec34d45c36f6fee7fb9427c490b032c';
 
 // Get the searchbar and buttons
-let button1 = document.getElementById("searchBtn");
-let button2 = document.getElementById("locationBtn");
-
-//Get Weather and Get Location buttons listeners
-button1.addEventListener("click", searchFutureWeather);
-button2.addEventListener("click", geoFindMe);
+const button1 = document.getElementById("searchBtn");
+const button2 = document.getElementById("locationBtn");
 
 // Get the modal
-var modal1 = document.getElementById("myModal1");
-var modal2 = document.getElementById("myModal2");
-var modal3 = document.getElementById("myModal3");
-var modal4 = document.getElementById("myModal4");
-//var modal5 = document.getElementById("myModal5");
+const modal1 = document.getElementById("myModal1");
+const modal2 = document.getElementById("myModal2");
+const modal3 = document.getElementById("myModal3");
+const modal4 = document.getElementById("myModal4");
 
 // Get the button that opens the modal
-var btn1 = document.getElementsByClassName("img")[0];
-var btn2 = document.getElementsByClassName("img")[1];
-var btn3 = document.getElementsByClassName("img")[2];
-var btn4 = document.getElementsByClassName("img")[3];
-//var btn5 = document.getElementsByClassName("img")[4];
+const btn1 = document.getElementsByClassName("img")[0];
+const btn2 = document.getElementsByClassName("img")[1];
+const btn3 = document.getElementsByClassName("img")[2];
+const btn4 = document.getElementsByClassName("img")[3];
 
 // Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close1")[0];
-var span2 = document.getElementsByClassName("close2")[0];
-var span3 = document.getElementsByClassName("close3")[0];
-var span4 = document.getElementsByClassName("close4")[0];
-//var span5 = document.getElementsByClassName("close5")[0];
+const span1 = document.getElementsByClassName("close1")[0];
+const span2 = document.getElementsByClassName("close2")[0];
+const span3 = document.getElementsByClassName("close3")[0];
+const span4 = document.getElementsByClassName("close4")[0];
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {modal1.style.display = "block";};
-btn2.onclick = function() {modal2.style.display = "block";};
-btn3.onclick = function() {modal3.style.display = "block";};
-btn4.onclick = function() {modal4.style.display = "block";};
-//btn5.onclick = function() {modal5.style.display = "block";};
+btn1.onclick = () => {modal1.style.display = "block";};
+btn2.onclick = () => {modal2.style.display = "block";};
+btn3.onclick = () => {modal3.style.display = "block";};
+btn4.onclick = () => {modal4.style.display = "block";};
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() {modal1.style.display = "none";};
-span2.onclick = function() {modal2.style.display = "none";};
-span3.onclick = function() {modal3.style.display = "none";};
-span4.onclick = function() {modal4.style.display = "none";};
-//span5.onclick = function() {modal5.style.display = "none";};
-
+span1.onclick = () => {modal1.style.display = "none";};
+span2.onclick = () => {modal2.style.display = "none";};
+span3.onclick = () => {modal3.style.display = "none";};
+span4.onclick = () => {modal4.style.display = "none";};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == modal1) {
       modal1.style.display = "none";
     }
@@ -59,16 +49,13 @@ window.onclick = function(event) {
     if (event.target == modal4) {
         modal4.style.display = "none";
     }
-    //if (event.target == modal5) {
-    //    modal5.style.display = "none";
-    //}
 };
 
 
 // function gets data from the API based on the searchTerm and 
 //calls drawWeatherPattern for (data, time, DOM element fraction of id or class)
 
-function searchFutureWeather() {
+const searchFutureWeather = () => {
     let searchTerm = document.getElementById("searchInput").value;
 
 	fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&mode=json&appid=${appId}`)  
@@ -82,7 +69,6 @@ function searchFutureWeather() {
         drawWeatherPattern(data, 16, '2nd');
         drawWeatherPattern(data, 24, '3rd');
         drawWeatherPattern(data, 32, '4th');
-        //drawWeatherPattern(data, 39, '5th');
 	})
 	.catch(function() {
 		// catch any errors
@@ -93,7 +79,7 @@ function searchFutureWeather() {
 //function triggered when "Get Location" button is clicked, 
 //gets coordinants and then calls getWeatherByLocation function
 
-function geoFindMe() {
+const geoFindMe = () => {
 
     const status = document.querySelector('#location');
     const mapLink = document.querySelector('#map-link');
@@ -106,7 +92,7 @@ function geoFindMe() {
       let roundLongitude = longitude.toPrecision([5]);
 
       //puts coordinants into the searchbar
-      document.getElementById("searchInput").value = `Cords:${roundLatitude}°/${roundLongitude}°`;
+      document.getElementById("searchInput").value = `${roundLatitude}°/${roundLongitude}°`;
 
       //calls getWeatherByLocation for recieved coords
       getWeatherByLocation(latitude, longitude);
@@ -117,23 +103,32 @@ function geoFindMe() {
        mapLink.innerHTML = `${roundLatitude} ° / ${roundLongitude} °`;
     }
   
-    function error() {
+    const error = () => {
         status.textContent = 'Unable to retrieve your location';
     }
   
     if (!navigator.geolocation) {
         status.textContent = 'Geolocation is not supported by your browser';
     } else {
-      status.textContent = 'Locating…';
+        status.textContent = 'Locating…';
         navigator.geolocation.getCurrentPosition(success, error);
     }
+};
 
-}
+//Get Weather and Get Location buttons listeners
+button1.addEventListener("click", searchFutureWeather);
+button2.addEventListener("click", geoFindMe);
+
+//Get Weather after typing in searchar and pressing ENTER
+document.getElementById("searchInput").addEventListener("keypress", (e) => {
+    if (e.key === 'Enter') {
+        searchFutureWeather();
+}});
 
 //function gets data from the API based on the coordinants from geoFindMe function
 //and calls drawWeatherPattern for (data, time, DOM element fraction of id or class)
 
-function getWeatherByLocation(lat, lon) {
+const getWeatherByLocation = (lat, lon) => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&mode=json&appid=${appId}`)  
 	.then(function(resp) { return resp.json() }) // Convert data to json
 	.then(function(data) {
@@ -146,7 +141,6 @@ function getWeatherByLocation(lat, lon) {
         drawWeatherPattern(data, 16, '2nd');
         drawWeatherPattern(data, 24, '3rd');
         drawWeatherPattern(data, 32, '4th');
-        //drawWeatherPattern(data, 39, '5th');
 	})
 	.catch(function() {
 		// catch any errors
@@ -157,9 +151,9 @@ function getWeatherByLocation(lat, lon) {
 //function which actually renders data to the DOM
 //parameters are (data, time, DOM element fraction of id or class)
 
-function drawWeatherPattern (d, time, dom) {
-    var celcius = Math.round(parseFloat(d.list[time].main.temp)-273.15);
-    var description = d.list[time].weather[0].main;
+const drawWeatherPattern = (d, time, dom) => {
+    let celcius = Math.round(parseFloat(d.list[time].main.temp)-273.15);
+    let description = d.list[time].weather[0].main;
     let picture;
 
     //find the weekday based on the date
@@ -188,5 +182,4 @@ function drawWeatherPattern (d, time, dom) {
     document.getElementById(`${dom}-humidity`).innerHTML = '<span class="iconify" data-icon="wi:humidity" data-inline="false"></span>' + d.list[time].main.humidity + ' %';
     document.getElementById(`${dom}-windspeed`).innerHTML = '<span class="iconify" data-icon="fa-solid:wind" data-inline="false"></span>' + d.list[time].wind.speed + ' m/s';
     document.getElementById(`${dom}-clouds`).innerHTML = '<span class="iconify" data-icon="uil:clouds" data-inline="false"></span>' + d.list[time].clouds.all + ' %';  
-}
-
+};
